a0826536383bcca113fa4a5d6e6b4a341ba5b73f
Woody230 <bselzer1@outlook.com>
2021-07-23 10:04:37 -0400

    Setup root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,40 @@
 plugins {
-    kotlin("multiplatform") version "1.4.10"
+    kotlin("multiplatform") version "1.5.10"
     id("com.android.library")
-    id("kotlin-android-extensions")
 }
 
-group = "com.bselzer.library.gw2"
-version = "1.0.0"
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
 
-repositories {
-    google()
-    jcenter()
-    mavenCentral()
+allprojects {
+    group = "com.bselzer.library.gw2"
+    version = "1.0.0"
+
+    apply(plugin = "maven-publish")
+
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+    }
 }
 
 kotlin {
-    android()
-    iosX64("ios") {
-        binaries {
-            framework {
-                baseName = "library"
-            }
-        }
-    }
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val androidMain by getting {
-            dependencies {
-                implementation("com.google.android.material:material:1.2.1")
-            }
-        }
-        val androidTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-                implementation("junit:junit:4.13")
-            }
-        }
-        val iosMain by getting
-        val iosTest by getting
+    android {
+        publishLibraryVariants("release", "debug")
     }
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
-        minSdkVersion(24)
-        targetSdkVersion(29)
+        minSdkVersion(23)
+        targetSdkVersion(30)
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
