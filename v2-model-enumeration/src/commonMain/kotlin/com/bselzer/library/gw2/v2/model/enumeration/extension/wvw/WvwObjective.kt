4a17ed50999404ccf810f733d54465949ae8c721
Woody230 <bselzer1@outlook.com>
2021-10-10 09:55:02 -0400

    Add map type extension

--- v2-model-enumeration/src/commonMain/kotlin/com/bselzer/library/gw2/v2/model/enumeration/extension/wvw/WvwObjective.kt
+++ v2-model-enumeration/src/commonMain/kotlin/com/bselzer/library/gw2/v2/model/enumeration/extension/wvw/WvwObjective.kt
@@ -1,5 +1,6 @@
 package com.bselzer.library.gw2.v2.model.enumeration.extension.wvw
 
+import com.bselzer.library.gw2.v2.model.enumeration.wvw.MapType
 import com.bselzer.library.gw2.v2.model.enumeration.wvw.ObjectiveType
 import com.bselzer.library.gw2.v2.model.wvw.objective.WvwObjective
 import com.bselzer.library.kotlin.extension.serialization.function.enumValueOrNull
@@ -8,3 +9,8 @@ import com.bselzer.library.kotlin.extension.serialization.function.enumValueOrNu
  * The [WvwObjective.type] as an [ObjectiveType]
  */
 fun WvwObjective.type(): ObjectiveType? = type.enumValueOrNull<ObjectiveType>()
+
+/**
+ * The [WvwObjective.mapType] as a [MapType]
+ */
+fun WvwObjective.mapType(): MapType? = mapType.enumValueOrNull<MapType>()
