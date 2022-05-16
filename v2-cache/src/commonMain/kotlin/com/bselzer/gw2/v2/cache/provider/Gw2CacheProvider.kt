077a88a4dad089735e802ffabc237cdb93053049
Woody230 <bselzer1@outlook.com>
2022-05-15 21:19:04 -0400

    Cache provider clear

--- v2-cache/src/commonMain/kotlin/com/bselzer/gw2/v2/cache/provider/Gw2CacheProvider.kt
+++ v2-cache/src/commonMain/kotlin/com/bselzer/gw2/v2/cache/provider/Gw2CacheProvider.kt
@@ -5,10 +5,18 @@ import com.bselzer.gw2.v2.cache.instance.GuildCache
 import com.bselzer.gw2.v2.cache.instance.WorldCache
 import com.bselzer.gw2.v2.cache.instance.WvwCache
 import com.bselzer.gw2.v2.client.client.Gw2Client
+import com.bselzer.ktx.kodein.db.transaction.Transaction
 
 class Gw2CacheProvider(client: Gw2Client) {
     val continent: ContinentCache = ContinentCache(client)
     val guild: GuildCache = GuildCache(client)
     val world: WorldCache = WorldCache(client)
     val wvw: WvwCache = WvwCache(client)
+
+    fun Transaction.clear() {
+        with(continent) { clear() }
+        with(guild) { clear() }
+        with(world) { clear() }
+        with(wvw) { clear() }
+    }
 }
