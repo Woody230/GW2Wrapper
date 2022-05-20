c3c6a972263db235e0c05b07a925975f0d0d277a
Woody230 <bselzer1@outlook.com>
2022-05-20 07:54:28 -0400

    Token as StringIdentifier

--- v2-client/src/commonMain/kotlin/com/bselzer/gw2/v2/client/model/Token.kt
+++ v2-client/src/commonMain/kotlin/com/bselzer/gw2/v2/client/model/Token.kt
@@ -1,8 +1,11 @@
 package com.bselzer.gw2.v2.client.model
 
+import com.bselzer.ktx.value.identifier.StringIdentifier
 import kotlinx.serialization.Serializable
 import kotlin.jvm.JvmInline
 
 @Serializable
 @JvmInline
-value class Token(val value: String)
+value class Token(override val value: String) : StringIdentifier {
+    override fun toString(): String = value
+}
