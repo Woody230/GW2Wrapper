0d455e06dc11a51aefe498ec367a6bceb4e77e39
Woody230 <bselzer1@outlook.com>
2022-05-22 14:59:45 -0400

    1. Publish multi variant 2. Updated serialization enumValue package

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -154,6 +154,13 @@ fun LibraryExtension.setup(manifestPath: String = "src/androidMain/AndroidManife
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    publishing {
+        multipleVariants {
+            allVariants()
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
     block(this)
 }
 
--- v2-model-enumeration/src/commonMain/kotlin/com/bselzer/gw2/v2/model/enumeration/extension/ConvertId.kt
+++ v2-model-enumeration/src/commonMain/kotlin/com/bselzer/gw2/v2/model/enumeration/extension/ConvertId.kt
@@ -8,7 +8,7 @@ import com.bselzer.gw2.v2.model.file.AssetId
 import com.bselzer.gw2.v2.model.legend.LegendId
 import com.bselzer.gw2.v2.model.profession.ProfessionId
 import com.bselzer.gw2.v2.model.race.RaceId
-import com.bselzer.ktx.serialization.function.enumValueOrNull
+import com.bselzer.ktx.serialization.json.function.enumValueOrNull
 
 fun AssetId.enumValueOrNull() = value.enumValueOrNull<AssetName>()
 fun ProfessionId.enumValueOrNull() = value.enumValueOrNull<ProfessionName>()
