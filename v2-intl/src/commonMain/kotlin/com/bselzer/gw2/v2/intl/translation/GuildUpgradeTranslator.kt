26d7e6cb40c7aafceaeb184a1328a850d641778c
Woody230 <bselzer1@outlook.com>
2022-05-25 15:36:58 -0400

    Guild upgrade cost translation

--- v2-intl/src/commonMain/kotlin/com/bselzer/gw2/v2/intl/translation/GuildUpgradeTranslator.kt
+++ v2-intl/src/commonMain/kotlin/com/bselzer/gw2/v2/intl/translation/GuildUpgradeTranslator.kt
@@ -2,6 +2,8 @@ package com.bselzer.gw2.v2.intl.translation
 
 import com.bselzer.gw2.v2.intl.model.Translations
 import com.bselzer.gw2.v2.model.guild.upgrade.GuildUpgrade
+import com.bselzer.gw2.v2.model.guild.upgrade.cost.CollectibleUpgradeCost
+import com.bselzer.gw2.v2.model.guild.upgrade.cost.ItemUpgradeCost
 import com.bselzer.ktx.function.collection.addTo
 
 class GuildUpgradeTranslator : Translator<GuildUpgrade> {
@@ -18,6 +20,20 @@ class GuildUpgradeTranslator : Translator<GuildUpgrade> {
             language = language
         ).addTo(this)
 
-        // TODO cost translations
+        default.costs.zip(translated.costs) { defaultCost, translatedCost ->
+            when {
+                defaultCost is CollectibleUpgradeCost && translatedCost is CollectibleUpgradeCost -> Translations(
+                    default = defaultCost.name,
+                    translated = translatedCost.name,
+                    language = language
+                ).addTo(this)
+                defaultCost is ItemUpgradeCost && translatedCost is ItemUpgradeCost -> Translations(
+                    default = defaultCost.name,
+                    translated = translatedCost.name,
+                    language = language
+                ).addTo(this)
+                else -> {}
+            }
+        }
     }
 }
