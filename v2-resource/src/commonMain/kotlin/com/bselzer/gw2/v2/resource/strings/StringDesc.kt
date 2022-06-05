f01778aa9aad7424130b18b5382dd01c9f4dd8a5
Woody230 <bselzer1@outlook.com>
2022-06-05 19:21:27 -0400

    Add resources

--- v2-resource/src/commonMain/kotlin/com/bselzer/gw2/v2/resource/strings/StringDesc.kt
+++ v2-resource/src/commonMain/kotlin/com/bselzer/gw2/v2/resource/strings/StringDesc.kt
@@ -2,6 +2,7 @@ package com.bselzer.gw2.v2.resource.strings
 
 import com.bselzer.gw2.v2.model.enumeration.WvwMapType
 import com.bselzer.gw2.v2.model.enumeration.WvwObjectiveOwner
+import com.bselzer.gw2.v2.model.enumeration.WvwObjectiveType
 import com.bselzer.gw2.v2.resource.Gw2Resources
 import com.bselzer.ktx.resource.KtxResources
 import dev.icerock.moko.resources.desc.StringDesc
@@ -20,4 +21,16 @@ fun WvwMapType.stringDesc(): StringDesc = when (this) {
     WvwMapType.BLUE_BORDERLANDS -> Gw2Resources.strings.blue_borderlands
     WvwMapType.GREEN_BORDERLANDS -> Gw2Resources.strings.green_borderlands
     WvwMapType.EDGE_OF_THE_MISTS -> Gw2Resources.strings.edge_of_the_mists
+}.desc()
+
+fun WvwObjectiveType.stringDesc(): StringDesc = when (this) {
+    WvwObjectiveType.CAMP -> Gw2Resources.strings.camp
+    WvwObjectiveType.CASTLE -> Gw2Resources.strings.castle
+    WvwObjectiveType.KEEP -> Gw2Resources.strings.keep
+    WvwObjectiveType.MERCENARY -> Gw2Resources.strings.mercenary
+    WvwObjectiveType.TOWER -> Gw2Resources.strings.tower
+    WvwObjectiveType.RUINS -> Gw2Resources.strings.ruins
+    WvwObjectiveType.RESOURCE -> Gw2Resources.strings.resource
+    WvwObjectiveType.GENERIC -> Gw2Resources.strings.generic
+    WvwObjectiveType.SPAWN -> Gw2Resources.strings.spawn
 }.desc()
