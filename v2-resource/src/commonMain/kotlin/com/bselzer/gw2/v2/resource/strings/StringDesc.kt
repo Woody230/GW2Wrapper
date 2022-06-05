3b4bfe9b53728e02ed6f45d8a015fcd07e03f1f8
Woody230 <bselzer1@outlook.com>
2022-06-05 15:14:16 -0400

    WvwMapType StringDesc

--- v2-resource/src/commonMain/kotlin/com/bselzer/gw2/v2/resource/strings/StringDesc.kt
+++ v2-resource/src/commonMain/kotlin/com/bselzer/gw2/v2/resource/strings/StringDesc.kt
@@ -1,17 +1,23 @@
 package com.bselzer.gw2.v2.resource.strings
 
+import com.bselzer.gw2.v2.model.enumeration.WvwMapType
 import com.bselzer.gw2.v2.model.enumeration.WvwObjectiveOwner
 import com.bselzer.gw2.v2.resource.Gw2Resources
 import com.bselzer.ktx.resource.KtxResources
 import dev.icerock.moko.resources.desc.StringDesc
 import dev.icerock.moko.resources.desc.desc
 
-/**
- * @return the [StringDesc] representing the objective owner
- */
 fun WvwObjectiveOwner.stringDesc(): StringDesc = when (this) {
     WvwObjectiveOwner.RED -> KtxResources.strings.red
     WvwObjectiveOwner.BLUE -> KtxResources.strings.blue
     WvwObjectiveOwner.GREEN -> KtxResources.strings.green
     WvwObjectiveOwner.NEUTRAL -> Gw2Resources.strings.neutral
+}.desc()
+
+fun WvwMapType.stringDesc(): StringDesc = when (this) {
+    WvwMapType.ETERNAL_BATTLEGROUNDS -> Gw2Resources.strings.eternal_battlegrounds
+    WvwMapType.RED_BORDERLANDS -> Gw2Resources.strings.red_borderlands
+    WvwMapType.BLUE_BORDERLANDS -> Gw2Resources.strings.blue_borderlands
+    WvwMapType.GREEN_BORDERLANDS -> Gw2Resources.strings.green_borderlands
+    WvwMapType.EDGE_OF_THE_MISTS -> Gw2Resources.strings.edge_of_the_mists
 }.desc()
