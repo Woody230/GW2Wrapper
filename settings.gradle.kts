89f9252bb5aa83dcb4663e989082c02cd2ef06d2
Woody230 <bselzer1@outlook.com>
2023-08-12 10:31:50 -0400

    revert bundled plugin usage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,18 @@
 pluginManagement {
     repositories {
-        google()
         gradlePluginPortal()
+        google()
         mavenCentral()
         mavenLocal()
     }
 }
 
 plugins {
-    id("io.github.woody230.gradle.internal.bundled") version "1.2.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
 }
+
+// TODO feature preview https://docs.gradle.org/8.1.1/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "GW2Wrapper"
 include("asset-cdn")
